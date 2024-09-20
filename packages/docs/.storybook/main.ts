ade6be2b2ad24396d888e50f8b8dcce56a3bcea9
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-09-20 01:54:21 -0300

    ci: add storybook deployer

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -26,5 +26,11 @@ const config: StorybookConfig = {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ds-ignite-ui/'
+    }
+    return config
+  }
 };
 export default config;
