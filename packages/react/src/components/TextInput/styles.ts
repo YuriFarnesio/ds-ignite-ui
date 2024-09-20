59394f68052630f1d4955b465e183add87483d5d
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-09-20 00:22:13 -0300

    feat: create text input component and its doc

---
+++ packages/react/src/components/TextInput/styles.ts
@@ -0,0 +1,49 @@
+import { styled } from '../../styles'
+
+export const TextInputContainer = styled('div', {
+  display: 'flex',
+  alignItems: 'baseline',
+  backgroundColor: '$gray900',
+  border: '2px solid $gray900',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  padding: '$3 $4',
+
+  '&:has(input:focus)': {
+    borderColor: '$ignite300',
+  },
+
+  '&:has(input:disabled)': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+})
+
+export const Prefix = styled('span', {
+  fontSize: '$sm',
+  fontFamily: '$default',
+  fontWeight: 'regular',
+  color: '$gray400',
+})
+
+export const Input = styled('input', {
+  width: '100%',
+  background: 'transparent',
+  fontSize: '$sm',
+  fontFamily: '$default',
+  fontWeight: 'regular',
+  color: '$white',
+  border: 0,
+
+  '&:focus': {
+    outline: 0,
+  },
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  '&:placeholder': {
+    color: '$gray400',
+  },
+})
