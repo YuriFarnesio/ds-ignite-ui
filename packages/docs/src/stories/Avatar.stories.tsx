48d64ae7c349a7c7c6d38b4752bdeaad07dfc404
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-09-20 00:47:49 -0300

    feat: add arg types to some component docs

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/YuriFarnesio.png',
     alt: 'Yuri Farnesio',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
