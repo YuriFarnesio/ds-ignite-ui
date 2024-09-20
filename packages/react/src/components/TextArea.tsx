3fef34697d65b2fbabf5464ba4eeab7767cf7410
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-09-20 00:24:46 -0300

    feat: create text area component and its doc

---
+++ packages/react/src/components/TextArea.tsx
@@ -0,0 +1,32 @@
+import { ComponentProps } from 'react'
+import { styled } from '../styles'
+
+export const TextArea = styled('textarea', {
+  minHeight: 80,
+  backgroundColor: '$gray900',
+  fontSize: '$sm',
+  fontFamily: '$default',
+  fontWeight: '$regular',
+  color: '$white',
+  resize: 'vertical',
+  border: '2px solid $gray900',
+  borderRadius: '$sm',
+  boxSizing: 'border-box',
+  padding: '$3 $4',
+
+  '&:focus': {
+    borderColor: '$ignite300',
+    outline: 0,
+  },
+
+  '&:disabled': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+
+  '&:placeholder': {
+    color: '$gray400',
+  },
+})
+
+export interface TextAreaProps extends ComponentProps<typeof TextArea> {}
